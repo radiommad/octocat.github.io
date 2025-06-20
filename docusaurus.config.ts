c38284f75192db5190eded0a3bc507f3f248a349
yguo <yguo@godaddy.com>
2025-06-20 16:05:29 -0700

    Test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,21 +15,19 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://radiommad.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'radiommad.github.io',
+  organizationName: 'radiommad',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
+  // useful metadata like html lang. For example, if your site is Chinese`, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
